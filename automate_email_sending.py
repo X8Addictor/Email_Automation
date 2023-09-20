47b082a8e33b18a9ad3db4a78e6832ce8d62ef40
X8Addictor <x8addictor@gmail.com>
2023-09-20 14:47:41 +0100

    Added docstrings and imporved logging method

--- automate_email_sending.py
+++ automate_email_sending.py
@@ -1,146 +1,226 @@
-''' You work at a company that sends daily reports to clients via email. The goal of this project is to automate the process of sending these reports via email.
-
-    Here are the steps you can take to automate this process:
-
-    Use the smtplib library to connect to the email server and send the emails.
-
-    Use the email library to compose the email, including the recipient's email address, the subject, and the body of the email.
-
-    Use the os library to access the report files that need to be sent.
-
-    Use a for loop to iterate through the list of recipients and send the email and attachment.
-
-    Use the schedule library to schedule the script to run daily at a specific time.
-
-    You can also set up a log file to keep track of the emails that have been sent and any errors that may have occurred during the email sending process. '''
-
 import smtplib
 import ssl
 import json
 import os
 import re
+import logging
 from getpass import getpass
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 
+# Define constants for file paths and directories.
 FILE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-CONFIG_DIRECTORY = os.path.join(FILE_DIRECTORY, 'config.json')
+CONFIG_DIRECTORY = os.path.join(FILE_DIRECTORY, 'Config')
+os.makedirs(CONFIG_DIRECTORY, exist_ok=True)
+CONFIG_FILE = os.path.join(CONFIG_DIRECTORY, 'config.json')
 LOG_DIRECTORY = os.path.join(FILE_DIRECTORY, 'Logs')
 os.makedirs(LOG_DIRECTORY, exist_ok=True)
-ERROR_FILE = os.path.join(LOG_DIRECTORY, 'Error.log')
+LOG_FILE = os.path.join(LOG_DIRECTORY, 'Logs.log')
+
+# Flags for controlling the printing of warnings and errors.
+error_flag = True
+warning_flag = True
+
+def setup_logging():
+    """Configure logging settings to save logs to a file."""
+    logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
 
 def get_recipients(filename):
+    """
+    Read recipient email addresses from a file and validate their format.
+
+    Args:
+        filename (str): The path to the file containing recipient email addresses.
+
+    Returns:
+        list: A list of valid recipient email addresses.
+        None: If no valid email addresses are found.
+    
+     Raises:
+        FileNotFoundError: If the recipients file is not found.
+        Exception: If an unexpected error occurs during processing.
+    """
     try:
         recipient_addresses = []
-        warning_flag = True
-
+        
         with open(filename, 'r') as recipients_file:
             for line in recipients_file:
-                recipient = sanitize_email(line.strip())
+                recipient = check_email_format(line.strip())
                 if recipient:
                     recipient_addresses.append(recipient)
                 else:
-                    log_file_path = os.path.join(LOG_DIRECTORY, 'invalid_recipient_addresses.log')
-
-                    if warning_flag:
-                        print(f'Warning: Invalid email address(s) found, check "{log_file_path}" for more info.')
-                        warning_flag = False
-                    
-                    with open(log_file_path, 'a') as log_file:
-                        log_file.write(f'Warning: Invalid email "{line.strip()}" in "{filename}."\n')
+                    log_warning(f'Invalid email "{line.strip()}" in "{filename}".')
 
         if not recipient_addresses:
             raise Exception(f'No valid email address(s) found in "{filename}".')
 
         return recipient_addresses
     except FileNotFoundError:
-        print(f'Error(s) occured, please check {ERROR_FILE} for more info')
-
-        with open(ERROR_FILE, 'a') as log_file:
-            log_file.write(f'Error: Recipients file "{filename}" not found.\n')
-
-        return []
+        log_error(f'Recipients file "{recipients_file}" not found.\n')
+        return None
     except Exception as e:
-        print(f'Error(s) occured, please check {ERROR_FILE} for more info')
+        log_error(f'{e}\n')
+        return None
 
-        with open(ERROR_FILE, 'a') as log_file:
-            log_file.write(f'Error: {e}\n')
+def check_email_format(email_address):
+    """
+    Check if an email address has a valid format.
 
-        return []
+    Args:
+        email_address (str): The email address to validate.
 
-def sanitize_email(email_address):
+    Returns:
+        str: The valid email address.
+        None: If the email address format is invalid.
+    """
     email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w{2,3}$'
 
     if re.match(email_pattern, email_address):
         return email_address
 
-def send_email(email_address, email_password, recipient_addresses, subject, message, attachment_path = None):
+def send_email(sender_address, sender_password, recipient_addresses, subject, message, attachment_path = ''):
+    """
+    Send an email to multiple recipients with optional attachments.
+
+    Args:
+        sender_address (str): The sender's email address.
+        sender_password (str): The sender's email password.
+        recipient_addresses (list): A list of recipient email addresses.
+        subject (str): The email subject.
+        message (str): The email body text.
+        attachment_path (str, optional): Path to an attachment file (default is None).
+
+    Returns:
+        bool: True if the email was sent successfully, False otherwise.
+
+    Raises:
+        Exception: If an unexpected error occurs during email sending.
+    """
     try:
-        if not email_address and not email_password:
-            raise Exception('Invalid email address and password.')
+        if not sender_address and not sender_password:
+            raise Exception(f'No email address and password found in "{CONFIG_FILE}".')
 
-        gmail_address = sanitize_email(email_address)
-
-        if not gmail_address:
-            raise Exception(f'Invalid gmail address format "{email_address}", please correct the gmail address in "config.json".')
-
-        gmail_password = email_password
+        if not check_email_format(sender_address):
+            raise Exception(f'Invalid gmail address format "{sender_address}", please edit the gmail address format in "{CONFIG_FILE}".')
 
         context = ssl.create_default_context()
 
         with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as gmail_server:
             gmail_server.login(gmail_address, gmail_password)
 
-            for recipient in recipient_addresses:
-                msg = compose_email(gmail_address, recipient, subject, message, attachment_path)
-                # gmail_server.sendmail(gmail_address, recipient, msg.as_string())
+            for recipient_address in recipient_addresses:
+                msg = compose_email(sender, recipient, subject, message, attachment_path)
 
-                log_file_path = os.path.join(LOG_DIRECTORY, 'successful_sent_emails.log')
-                with open(log_file_path, 'a') as log_file:
-                    log_file.write(f'Success: Email sent to "{recipient.strip()}"\n')
+                if msg is not None:
+                    server.sendmail(sender, recipient, message.as_string())
+                    log_success(f'Successfully sent email to "{recipient_address.strip()}"\n')
 
         return True
     except Exception as e:
-        print(f'Error(s) occured, please check {ERROR_FILE} for more info')
-
-        log_file_path = os.path.join(LOG_DIRECTORY, 'failed_sent_emails.log')
-        with open(log_file_path, 'a') as log_file:
-            log_file.write(f'Error: {e}\n')
-
-        print(f'Error: {e}')
+        log_error(f'{e}\n')
         return False
         
-def compose_email(email_address, recipient_address, subject, message, attachment_path = None):
-    msg = MIMEMultipart()
-    msg['From'] = email_address
-    msg['To'] = recipient_address
-    msg['Subject'] = subject
+def compose_email(email_address, recipient_address, subject, message, attachment_path = ''):
+    """
+    Compose an email message with optional attachments.
 
-    msg.attach(MIMEText(message, 'plain'))
+    Args:
+        email_address (str): The sender's email address.
+        recipient_address (str): The recipient's email address.
+        subject (str): The email subject.
+        message (str): The email body text.
+        attachment_path (str, optional): Path to an attachment file (default is None).
 
-    if os.path.exists(attachment_path):
-        with open(attachment_path, 'rb') as attachment_file:
-            part = MIMEApplication(attachment_file.read(), Name=os.path.basename(attachment_path))
-            part['Content-Disposition'] = f'attachment; filename="{os.path.basename(attachment_path)}"'
-            msg.attach(part)
-    else:
-        print(f'Error(s) occured, please check {ERROR_FILE} for more info')
+    Returns:
+        email.message.Message: The composed email message.
+        None: If an attachment file is specified but not found.
 
-        log_file_path = os.path.join(LOG_DIRECTORY, 'failed_email_attachments.log')
-        with open(log_file_path, 'a') as log_file:
-            log_file.write(f'Error: {attachment_file} not found.\n')
+    Raises:
+        FileExistsError: If the specified attachment file does not exist.
+    """
+    try:
+        msg = MIMEMultipart()
+        msg['From'] = email_address
+        msg['To'] = recipient_address
+        msg['Subject'] = subject
 
-    return msg
+        msg.attach(MIMEText(message, 'plain'))
 
-def main(email_address, email_password, recipients_file, subject, message, attachment_path = None):
+        if os.path.exists(attachment_path):
+            with open(attachment_path, 'rb') as attachment_file:
+                part = MIMEApplication(attachment_file.read(), Name=os.path.basename(attachment_path))
+                part['Content-Disposition'] = f'attachment; filename="{os.path.basename(attachment_path)}"'
+                msg.attach(part)
+        elif not os.path.exists(attachment_path) and attachment_path is not '':
+            raise FileExistsError
+
+        return msg
+    except FileExistsError:
+        log_error(f'Attachment file "{attachment_path}" not found.\n')
+        return None
+
+def log_warning(message):
+    """
+    Log a warning message and print it to the console.
+
+    Args:
+        message (str): The warning message.
+    """
+    global warning_flag
+
+    if warning_flag:
+        print(f'Warning: Invalid email address(es) found "{filename}", check "{LOG_FILE}" for more info.')
+        warning_flag = False
+
+    logging.warning(message)    
+
+def log_error(message):
+    """
+    Log an error message and print it to the console.
+
+    Args:
+        message (str): The error message.
+    """
+    global error_flag
+
+    if error_flag:
+        print(f'Error(s) occured, please check "{LOG_FILE}" for more details.')
+        error_flag = False
+
+    logging.error(message)
+
+def log_success(message):
+    """
+    Log a success message.
+
+    Args:
+        message (str): The success message.
+    """
+    logging.info(message)
+
+def main(email_address, email_password, recipients_file, subject, message, attachment_path = ''):
+    """
+    Main function to send emails to recipients.
+
+    Args:
+        email_address (str): The sender's email address.
+        email_password (str): The sender's email password.
+        recipients_file (str): Path to the file containing recipient email addresses.
+        subject (str): The email subject.
+        message (str): The email body text.
+        attachment_path (str, optional): Path to an attachment file (default is None).
+    """
     recipient_email_addresses = get_recipients(recipients_file)
 
     if recipient_email_addresses:
         success = send_email(email_address, email_password, recipient_email_addresses, subject, message, attachment_path)
 
+
 if __name__ == '__main__':
-    with open(CONFIG_DIRECTORY, 'r') as f:
+    setup_logging()
+
+    with open(CONFIG_FILE, 'r') as f:
         config = json.load(f)
 
     email_address = config.get('email_address', '')
@@ -148,5 +228,6 @@ if __name__ == '__main__':
     recipients_file = config.get('recipients_file', '')
     subject = config.get('subject', '')
     message = config.get('message', '')
-    attachment_path = config.get('attachment_path')  # This can be None
+    attachment_path = config.get('attachment_path')  # This can be empty
+
     main(email_address, email_password, recipients_file, subject, message, attachment_path)
